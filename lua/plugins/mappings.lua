d3dda50a46f4c178771540b43f518da1969a29e9
matze <104998443+prodmatze@users.noreply.github.com>
2024-11-09 17:16:09 +0100

    added filepath completion to cmp

--- lua/plugins/mappings.lua
+++ lua/plugins/mappings.lua
@@ -85,6 +85,7 @@ return {
         sources = cmp.config.sources({
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
+          { name = 'path' },
         }, {
           { name = 'buffer' },
         })
